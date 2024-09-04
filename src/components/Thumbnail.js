0e22846c67454b7777e0f0643ec754972ca811ef
cbidici <cbidici@gmail.com>
2024-09-05 00:04:49 +0100

    Thumbnail view fixes

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,18 +1,13 @@
 import {useState} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Thumbnail.css';
 
 function Thumbnail({resource}) {
   const [isThumbnailLoading, setThumbnailLoading] = useState(true);
-  const navigate = useNavigate();
 
   const onThumbnailLoad = () => {
     setThumbnailLoading(false);
   };
-
-  function preview(resourceId) {
-    navigate('/preview/'+resourceId);
-  }
 
   let thumbnail;
   if(resource.type === 'DIRECTORY') {
@@ -24,7 +19,7 @@ function Thumbnail({resource}) {
       </Link>
   } else if(resource.type.startsWith('IMAGE')) {
     thumbnail =
-      <button type="button" disabled={isThumbnailLoading} onClick={() => preview(resource.id)}>
+      <Link disabled={isThumbnailLoading} to={'/preview/'+resource.id}>
         <img display={isThumbnailLoading ? "none" : "block"} src={process.env.REACT_APP_FILE_SERVICE_HOST+'/thumbnails/'+resource.id} alt="" onLoad={onThumbnailLoad} onError={onThumbnailLoad} />
         {isThumbnailLoading &&
           <div style={{width:"100%", height:"100%"}}>
@@ -35,10 +30,10 @@ function Thumbnail({resource}) {
             </div>
           </div>
         }
-      </button>
+      </Link>
   } else if(resource.type.startsWith("VIDEO")) {
     thumbnail =
-      <button type="button" disabled={isThumbnailLoading} onClick={() => preview(resource.id)}>
+      <Link disabled={isThumbnailLoading} to={'/preview/'+resource.id}>
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-play-circle play" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
           <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
@@ -53,7 +48,7 @@ function Thumbnail({resource}) {
             </div>
           </div>
         }
-      </button>
+      </Link>
   }
 
   return (
